04cf387a67696a5af12149ad98408c970cdc1654
David Pierre <davidpierrealves21@gmail.com>
2023-07-31 14:14:21 -0300

    Update settings for different environments

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -1,3 +1,5 @@
+import os
+
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
@@ -6,11 +8,11 @@ load_dotenv('.env.prod')
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-    ENVIRONMENT: str
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM: str = os.getenv("ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+    ENVIRONMENT: str = os.getenv("ENVIRONMENT")
 
 
 settings = Settings()
